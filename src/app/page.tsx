8816855a0e40f25b96b077c3b56795e330fe9b7e
Hasan <120580726+h0dy@users.noreply.github.com>
2025-03-03 21:51:23 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <section className="h-screen w-screen grid place-items-center">
       <div className="flex flex-col items-center gap-y-2">
-        <h1 className="capitalize">coming soon...</h1>
+        <h1 className="capitalize">coming soon</h1>
         <Loader />
       </div>
     </section>
